8d5753314e3bd96949d01b81353485b3b71eb11d
Samuel Gagnon-Hartman <sgagnonhartman@gmail.com>
2024-02-14 16:38:34 +0100

    added validation loss and early stopping

--- train.py
+++ train.py
@@ -38,10 +38,48 @@ logger = logging.getLogger()
 logger.addHandler(handler)
 logger.setLevel('INFO')
 
-def train_one_epoch():
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=False, delta=0, checkpoint_path=checkpoint_dir):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = -1e10
+        self.delta = delta
+        self.checkpoint_path = checkpoint_path
+
+    def __call__(self, val_loss, model, optimizer, ema, epoch):
+
+        score = val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, optimizer, ema, epoch)
+        elif score > self.best_score + self.delta:
+            self.counter += 1
+            logging.info(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, optimizer, ema, epoch)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model, optimizer, ema, epoch):
+        '''Saves model when validation loss decreases.'''
+        if self.verbose:
+            logging.info(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(
+            dict(optimizer=optimizer.state_dict(), model=model.module.state_dict(), ema=ema.state_dict(), epoch=epoch),
+            os.path.join(self.checkpoint_path, f'best_checkpoint.pth')
+            )
+        self.val_loss_min = val_loss
+
+def train_one_epoch(data_loader, model, optimizer, ema, train_or_val='training'):
     avg_loss = 0.
     counter = 0
-    for i, data_list in enumerate(training_loader):
+    for i, data_list in enumerate(data_loader):
         input_data = data_list[0].to(DEVICE)
         label_data = data_list[1].to(DEVICE)
         
@@ -55,20 +93,25 @@ def train_one_epoch():
         sigmas = sigma_time(time_steps).to(DEVICE)
         sigmas = sigmas[:,None,None,None,None]
 
-
         # Generate noise perturbed input
         z = torch.randn_like(label_data,  device=DEVICE)
         inputs = torch.cat([label_data + sigmas * z, input_data], dim=1)
 
-        optimizer.zero_grad()
-        output = model(inputs, time_steps)
-
-        # Optimize with score matching loss
-        loss = torch.sum(torch.square(output + z)) /  B
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), config.optim.grad_clip)
-        optimizer.step()
-        ema.update()
+        # backpropagation if training, otherwise just compute loss
+        if train_or_val == 'training':
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            output = model(inputs, time_steps)
+            # Optimize with score matching loss
+            loss = torch.sum(torch.square(output + z)) /  B
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), config.optim.grad_clip)
+            optimizer.step()
+            ema.update()
+        elif train_or_val == 'validation':
+            with torch.no_grad():
+                output = model(inputs, time_steps)
+                loss = torch.sum(torch.square(output + z)) /  B
         avg_loss += loss.item()
         counter += 1
     return avg_loss/counter
@@ -78,6 +121,10 @@ model = DataParallel(UNet3DModel(config))
 model = model.to(DEVICE)
 
 # Define optimizer
+# Adam converges faster than SGD, but SGD finds better solutions
+# for this problem. We use Adam to get close to the solution and
+# then switch to SGD to get the best solution.
+# optimizer = torch.optim.SGD(model.parameters(), lr=config.optim.lr, momentum=0.9, weight_decay=config.optim.weight_decay)
 optimizer = torch.optim.Adam(
         model.parameters(),
         lr=config.optim.lr,
@@ -106,20 +153,27 @@ init_epoch = 0
 scratch_ddir = '/leonardo_scratch/large/userexternal/sgagnonh/diff_data/diff_data/galaxies/'
 training_dataset = GalaxyDataset(datadir=scratch_ddir, job_type=args.j, train_or_val='training', single_nf=0.4)
 training_loader = DataLoader(training_dataset, config.training.batch_size, shuffle=True, num_workers=args.num_workers)
-# validation_dataset = GalaxyDataset(datadir=scratch_ddir, job_type='galsmear', train_or_val='validation', single_ion_eff=True)
-# validation_loader = DataLoader(validation_dataset, config.training.batch_size, shuffle=True, num_workers=1)
+validation_dataset = GalaxyDataset(datadir=scratch_ddir, job_type=args.j, train_or_val='validation', single_nf=0.4)
+validation_loader = DataLoader(validation_dataset, config.training.batch_size, shuffle=True, num_workers=args.num_workers)
 
 model.train(True)
+
+early_stopping = EarlyStopping(patience=5, verbose=True, checkpoint_path=checkpoint_dir)
 
 logging.info('Starting training loop.')
 for epoch in range(init_epoch, config.training.n_epochs + 1):
     
     # TODO pass arguments to train_one_epoch
     # rather than using global variables
-    avg_loss = train_one_epoch()
+    avg_loss = train_one_epoch(training_loader, model, optimizer, ema, train_or_val='training')
     
     if epoch % 10 == 0:
-        logging.info('epoch: {}, training loss: {}'.format(epoch+1, avg_loss))
+        val_loss = train_one_epoch(training_loader, model, optimizer, ema, train_or_val='validation')
+        logging.info(f'epoch: {epoch+1}, training loss: {avg_loss}, validation loss: {val_loss}')
+        early_stopping(val_loss, model, optimizer, ema, epoch)
+        if early_stopping.early_stop:
+            logging.info("Early stopping")
+            break
         torch.save(
             dict(optimizer=optimizer.state_dict(), model=model.module.state_dict(), ema=ema.state_dict(), epoch=epoch),
             os.path.join(checkpoint_dir, f'checkpoint_{epoch}.pth')

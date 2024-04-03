2f2a059be7dd1becf98968a8bc558c2d3a39b781
Samuel Gagnon-Hartman <sgagnonhartman@gmail.com>
2024-04-03 11:52:17 +0200

    training script

--- train.sh
+++ train.sh
@@ -18,6 +18,6 @@
 module load profile/deeplrn
 source ~/ddpm/bin/activate
 
-python train.py -j galsmear --num_workers 2 --num_bins 3
-python sample.py -j galsmear --num_workers 2 --num_bins 3 --task_id training
-python sample.py -j galsmear --num_workers 2 --num_bins 3 --task_id validation
+python train.py -j galsmear --num_workers 2 --num_bins 1
+python sample.py -j galsmear --num_workers 2 --num_bins 1 --task_id training
+python sample.py -j galsmear --num_workers 2 --num_bins 1 --task_id validation
